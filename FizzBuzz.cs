f417737206e95a39ffd3bf07994e4b7c6d4fcc0b
Даниил Клименков <klimenkokov1@gmail.com>
2024-09-23 13:32:14 +0300

    Squashed 'Задание 5/' content from commit 5e9ee6e
    git-subtree-dir: Задание 5
    git-subtree-split: 5e9ee6e470b6b713db12e68679878d3c230c8671

---
+++ FizzBuzz.cs
@@ -0,0 +1,45 @@
+﻿namespace FizzBuzz
+{
+   internal class FizzBuzz
+   {
+      static void Main(string[] args)
+      {
+         FizzBuzz fizzBuzz = new FizzBuzz();
+         fizzBuzz.Start();
+      }
+
+
+
+      #region Точка начала
+      private void Start()
+      {
+         for (int i = 1; i <= 100; i++)
+         {
+            Console.WriteLine(Condition(i));
+         }
+      }
+      #endregion
+
+      #region Условия вывода
+      private string Condition(int number)
+      {
+         if (number % 3 == 0 && number % 5 == 0)
+         {
+            return "FizzBuzz";
+         }
+         else if (number % 5 == 0)
+         {
+            return "Buzz";
+         }
+         else if (number % 3 == 0)
+         {
+            return "Fizz";
+         }
+         else
+         {
+            return number.ToString();
+         }
+      }
+      #endregion
+   }
+}
