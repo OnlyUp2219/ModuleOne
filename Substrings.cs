5303809cb45c4c41e545eba85da4eced3d0ebf3b
Даниил Клименков <klimenkokov1@gmail.com>
2024-09-23 13:32:08 +0300

    Squashed 'Задание 3/' content from commit c456eac
    git-subtree-dir: Задание 3
    git-subtree-split: c456eac712a9b2c08edf91d8dce86e7bce5c334d

---
+++ Substrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Substrings
+{
+   internal class Substrings
+   {
+      static void Main(string[] args)
+      {
+         Substrings substrings = new Substrings();
+         substrings.Start();
+      }
+
+      #region Точка входа в приложение
+      private void Start()
+      {
+         Console.WriteLine("Введите строку:");
+         string? strMain = Console.ReadLine();
+         Console.WriteLine("Введите подстроку:");
+         string? strSub = Console.ReadLine();
+
+         bool result = isSubstrings(strMain, strSub);
+         Console.WriteLine(result
+             ? $"\"{strSub}\" является подстрокой \"{strMain}\"."
+             : $"\"{strSub}\" не является подстрокой \"{strMain}\".");
+      }
+      #endregion
+
+      #region Проверка на подстроку
+      private bool isSubstrings(string strMain, string strSub)
+      {
+         return strMain.Contains(strSub);
+      }
+      #endregion
+   }
+}
