b600d8708e7b480d67c3cfd4beb66ea6d26e722d
Даниил Клименков <klimenkokov1@gmail.com>
2024-09-25 11:35:38 +0300

    Добавил регионы и комменты

--- МОДУЛЬ 1.3/Задание 2.2/MinElementSumArray.cs
+++ МОДУЛЬ 1.3/Задание 2.2/MinElementSumArray.cs
@@ -1,9 +1,5 @@
-﻿/*
-Присваивая последовательным элементам массива случайные значения от 1 до 9, 
-создать массив с минимальным количеством элементов, 
-сумма которых не превышает заданного пользователем числа.
-*/
-
+﻿using System;
+using System.Collections.Generic;
 
 namespace MinElementSumArray
 {
@@ -13,48 +9,66 @@ namespace MinElementSumArray
       {
          MinElementSumArray minElementSumArray = new MinElementSumArray();
          minElementSumArray.Start();
-
       }
 
+      #region Старт программы
+      /// <summary>
+      /// Старт программы. Ввод числа от пользователя и создание массива.
+      /// </summary>
       private void Start()
       {
          Console.WriteLine("Введите число!");
          int value;
-         int.TryParse(Console.ReadLine(), out value);
-         List<int> list = FillArrayWithLimit(value);
+         int.TryParse(Console.ReadLine(), out value); // Преобразование строки в число
+         List<int> list = FillArrayWithLimit(value);  // Заполнение массива
          Console.WriteLine("Массив: ");
-         Print(list);
+         Print(list);  // Вывод массива
       }
+      #endregion
 
+      #region Заполнение массива
+      /// <summary>
+      /// Заполняет массив случайными числами от 1 до 9, 
+      /// пока их сумма не превысит заданное пользователем число.
+      /// </summary>
+      /// <param name="value">Максимальная допустимая сумма элементов массива</param>
+      /// <returns>Массив чисел</returns>
       private List<int> FillArrayWithLimit(int value)
       {
-         List<int> list = new List<int>();
-         Random random = new Random();
-         int currentSum = 0; // Текущая сумма элементов массива
+         List<int> list = new List<int>();  // Инициализация пустого списка
+         Random random = new Random();  // Генератор случайных чисел
+         int currentSum = 0;  // Текущая сумма элементов массива
+
+         // Заполняем массив до тех пор, пока сумма элементов не превысит заданное значение
          for (int i = 0; i < value; i++)
          {
-            int rndValue = random.Next(1, 10);
-            if (currentSum <= value)
+            int rndValue = random.Next(1, 10);  // Случайное число от 1 до 9
+            if (currentSum + rndValue <= value) // Если текущая сумма с новым числом не превышает лимит
             {
-               currentSum += rndValue;
-               list.Add(rndValue);
+               currentSum += rndValue;  // Добавляем значение к сумме
+               list.Add(rndValue);      // Добавляем элемент в массив
             }
             else
-               return list;
+               return list;  // Если сумма превышена, возвращаем текущий массив
          }
          return list;
       }
+      #endregion
 
+      #region Вывод массива
+      /// <summary>
+      /// Печатает элементы списка в консоль.
+      /// </summary>
+      /// <param name="list">Список чисел для вывода</param>
       private void Print(List<int> list)
       {
-            Console.Write("[ ");
+         Console.Write("[ ");
          foreach (int i in list)
          {
-                Console.Write(i + " ");
+            Console.Write(i + " ");  // Выводим каждый элемент массива
          }
          Console.WriteLine("]");
       }
-
-
+      #endregion
    }
 }
