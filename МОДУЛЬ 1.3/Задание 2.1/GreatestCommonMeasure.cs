215278112a4a2219570afea344139607a8663e7f
Даниил Клименков <klimenkokov1@gmail.com>
2024-09-25 11:11:04 +0300

    Добавил регионы и комментарии

--- МОДУЛЬ 1.3/Задание 2.1/GreatestCommonMeasure.cs
+++ МОДУЛЬ 1.3/Задание 2.1/GreatestCommonMeasure.cs
@@ -1,12 +1,4 @@
-﻿/*
-   Определить функцию(статический метод) для вычисления наибольшего общего делителя двух целых натуральных чисел (Greatest Common Measure). 
-  В основной программе, используя функцию, сократить неотрицательную обыкновенную дробь. 
-  Дробь вводится с клавиатуры в виде неотрицательного числителя и положительного знаменателя
-*/
-
-
-
-using System;
+﻿using System;
 
 namespace GreatestCommonMeasure
 {
@@ -15,29 +7,52 @@ namespace GreatestCommonMeasure
       static void Main(string[] args)
       {
          GreatestCommonMeasure greatestCommonMeasure = new GreatestCommonMeasure();
-         Console.WriteLine("Введите дробь в виде неотрицательного числителя и положительного знаменателя  (пример 36/48)");
+
+         // Ввод дроби от пользователя
+         Console.WriteLine("Введите дробь в виде неотрицательного числителя и положительного знаменателя (пример 36/48)");
          string? strValue = Console.ReadLine();
+
+         // Преобразование введенной дроби в числитель и знаменатель
          int[] intValues = greatestCommonMeasure.GetValueFromFraction(strValue);
+
+         // Вычисление НОД (наибольшего общего делителя)
          int GCD = FindGCD(intValues[0], intValues[1]);
          Console.WriteLine($"НОД = {GCD}");
+
+         // Сокращение дроби
          string ReducedFraction = greatestCommonMeasure.GetReducedFraction(intValues, GCD);
-         Console.WriteLine("Сокращенная дробь " + ReducedFraction);
+         Console.WriteLine("Сокращенная дробь: " + ReducedFraction);
       }
 
+      #region Преобразование дроби
+
+      /// <summary>
+      /// Метод для сокращения дроби с использованием НОД.
+      /// </summary>
+      /// <param name="intValues">Массив с числителем и знаменателем</param>
+      /// <param name="GCD">Наибольший общий делитель</param>
+      /// <returns>Сокращенная дробь в виде строки</returns>
       private string GetReducedFraction(int[] intValues, int GCD)
       {
          for (int i = 0; i < intValues.Length; i++)
          {
             intValues[i] = intValues[i] / GCD;
          }
-
-
          return $"{intValues[0]}/{intValues[1]}";
       }
 
+      #endregion
+
+      #region Парсинг строки дроби
+
+      /// <summary>
+      /// Метод для извлечения числителя и знаменателя из строки вида "числитель/знаменатель".
+      /// </summary>
+      /// <param name="strValue">Введенная пользователем строка</param>
+      /// <returns>Массив, где [0] - числитель, [1] - знаменатель</returns>
       private int[] GetValueFromFraction(string strValue)
       {
-         string[] values = (strValue.Split('/', StringSplitOptions.RemoveEmptyEntries));
+         string[] values = strValue.Split('/', StringSplitOptions.RemoveEmptyEntries);
          int[] intValues = new int[2];
          for (int i = 0; i < 2; i++)
          {
@@ -47,7 +62,17 @@ namespace GreatestCommonMeasure
          return intValues;
       }
 
-      #region НОД
+      #endregion
+
+      #region НОД (Наибольший общий делитель)
+
+      /// <summary>
+      /// Метод для нахождения наибольшего общего делителя (НОД) двух чисел.
+      /// Используется рекурсивный алгоритм Евклида.
+      /// </summary>
+      /// <param name="a">Первое число</param>
+      /// <param name="b">Второе число</param>
+      /// <returns>Наибольший общий делитель</returns>
       static int FindGCD(int a, int b)
       {
          while (true)
@@ -65,9 +90,8 @@ namespace GreatestCommonMeasure
                return FindGCD(a, b - a);
             }
          }
-      } 
+      }
+
       #endregion
-
-
    }
 }
